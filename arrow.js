0211cd16414690d72de4b91baef95946cb705227
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-08 23:58:28 +0500

    refactor: Update arrow.js #1

---
+++ arrow.js
@@ -0,0 +1,8 @@
+//* This file is only to learn & practise arrow functions
+
+//* one line statement without "return"
+var printName = (name) => name;
+console.log(printName("Hey, I am peter!"));
+
+var getNumber = () => 42;
+console.log(getNumber());
