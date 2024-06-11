f5c06b8762682d101d019053677761758a3b2a11
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-11 20:55:47 +0500

    refactor #4

--- arrow.js
+++ arrow.js
@@ -1,54 +1,85 @@
 //* This file is only to learn & practise arrow functions
 
 //* one line statement without "return" & atleast 1 parameter
-var printName = (name) => name;
-console.log(printName("Hey, I am peter!"));
+// var printName = (name) => name;
+// console.log(printName("Hey, I am peter!"));
 
 //* one line statement without "return" no parameter
-var getNumber = () => 42;
-console.log(getNumber());
+// var getNumber = () => 42;
+// console.log(getNumber());
 
 //* one line statement without "return" & 2 parameter
-var calculatePrice = (quantity, tax) => quantity * 5 * (tax + 1);
-console.log(calculatePrice(5, 0.95));
+// var calculatePrice = (quantity, tax) => quantity * 5 * (tax + 1);
+// console.log(calculatePrice(5, 0.95));
 
 //* more than 1 line statement and with "return" statement
-var calculatePrice = (quantity, tax) => {
-  let price = quantity * 5;
-  price *= tax + 1;
-  return price;
-};
+// var calculatePrice = (quantity, tax) => {
+//   let price = quantity * 5;
+//   price *= tax + 1;
+//   return price;
+// };
 
-console.log(calculatePrice(5, 0.95));
+// console.log(calculatePrice(5, 0.95));
 
 //* Using `()` in arrow function to return an "object literal"
-var getNumber = (data) => ({ data: data, number: 42 });
-console.log(getNumber("check"));
+// var getNumber = (data) => ({ data: data, number: 42 });
+// console.log(getNumber("check"));
 
 //* or Equivalently
-var getNumber = function (data) {
-  return {
-    data: data,
-    number: 42,
-  };
-};
-console.log(getNumber("uncheck"));
+// var getNumber = function (data) {
+//   return {
+//     data: data,
+//     number: 42,
+//   };
+// };
+// console.log(getNumber("uncheck"));
 
 //* Using arrow functions to create IIEF!
 
-var fn1 = (function (number) {
-  return {
-    getNumber: function () {
-      return number;
-    },
-  };
-})(44);
-console.log(fn1.getNumber());
+// var fn1 = (function (number) {
+//   return {
+//     getNumber: function () {
+//       return number;
+//     },
+//   };
+// })(44);
+// console.log(fn1.getNumber());
 
-var fn2 = (function printName(name) {
-  return function getName() {
-    return name;
-  };
-})("Anusha");
+// var fn2 = (function printName(name) {
+//   return function getName() {
+//     return name;
+//   };
+// })("Anusha");
 
-console.log(fn2());
+// console.log(fn2());
+
+//* Tale about "this"
+
+//* Function Invocation
+// function taleAboutThis() {
+//   console.log(this);
+// }
+// taleAboutThis();
+// if (window == this) {
+//   console.log("this refers to the Global Context!");
+// }
+
+//* Metjod Invocation
+let obj = {
+  name: "fancy",
+  operation: function () {
+    console.log(this);
+  },
+};
+
+let obj2 = {
+  name: "Fairy",
+};
+
+// obj.operation(); //* method invocation
+
+// let x = obj.operation;
+// x(); //* function invocation
+
+let operation = obj.operation;
+operation.call(obj2);
